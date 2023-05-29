363f3a13f1a49b90f2189117edaf6f835b6849c7
NazarovDA <d.n.0@bk.ru>
2023-05-29 13:12:02 +0300

    config changed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ["~/assets/css/main.css"],
-  modules: [
-    'nuxt-simple-sitemap',
-  ],
-  
+  modules: ["nuxt-simple-sitemap", "@artmizu/yandex-metrika-nuxt"],
+  runtimeConfig: { yandexMetrika: { id: 93755986 } },
 });
