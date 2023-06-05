c97ec0b837bc10963d046afb83769dd12bad6962
NazarovDA <d.n.0@bk.ru>
2023-06-05 15:25:18 +0300

    404 handling

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,3 +1,12 @@
 <template>
-  404
+  <div class="content">
+    <div class="content-up-stripe"></div>
+    <div class="content-wall">
+      <div class="breadcrumb">
+        <p id="breadcrumbs"><span class="breadcrumb_last">Ошибка 404</span></p>
+      </div>
+      Страница не найдена.
+    </div>
+    <div class="content-down-stripe"></div>
+  </div>
 </template>
