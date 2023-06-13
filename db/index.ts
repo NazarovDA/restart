faed7e060477d510b482e2e57eac101fe4a326c3
NazarovDA <d.n.0@bk.ru>
2023-06-13 17:39:35 +0300

    db url update

--- db/index.ts
+++ db/index.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 export const prismaClient = new PrismaClient({
   datasources: {
     db: {
-      url: process.cwd().includes(".output") ? "file:../../../../../prisma/dev.db" : "file:./dev.db",
+      url: process.env.DB_URL,
     },
   },
 });
