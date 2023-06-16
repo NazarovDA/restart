713e9e7a4448bdde7d4aed61cc3ca5cccb50b542
NazarovDA <d.n.0@bk.ru>
2023-06-16 22:26:27 +0300

    error handling

---
+++ error.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const err = useError();
+
+const err_code = (err.value as any)?.statusCode;
+</script>
+
+<template>
+  <NuxtLayout name="default">
+    <div class="content">
+      <div class="content-up-stripe"></div>
+      <div class="content-wall">
+        <div class="breadcrumb">
+          <p id="breadcrumbs">
+            <span class="breadcrumb_last">Ошибка {{ err_code }}</span>
+          </p>
+        </div>
+        <span v-if="err_code == 404">Страница не найдена.</span>
+      </div>
+      <div class="content-down-stripe"></div>
+    </div>
+  </NuxtLayout>
+</template>
---
+++ pages/404.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="content">
+      <div class="content-up-stripe"></div>
+      <div class="content-wall">
+        <div class="breadcrumb">
+          <p id="breadcrumbs"><span class="breadcrumb_last">Ошибка 404</span></p>
+        </div>
+        Страница не найдена.
+      </div>
+      <div class="content-down-stripe"></div>
+    </div>
+  </template>
+  
