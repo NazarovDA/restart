395772afd205d3c127f11311c21d9d945787f60b
NazarovDA <d.n.0@bk.ru>
2023-07-17 09:32:28 +0300

    products

---
+++ pages/admin/product/[id].vue
@@ -0,0 +1,199 @@
+<script setup lang="ts">
+import { Product } from "types/product";
+// import cuid from "cuid";
+
+import InputText from "primevue/inputtext";
+// import TreeSelect from "primevue/treeselect";
+import FileUpload from "primevue/fileupload";
+import Image from "primevue/image";
+import Dropdown from "primevue/dropdown";
+import Button from "primevue/button";
+import BlockUI from "primevue/blockui";
+
+import { FileUploadRemoveUploadedFile } from "primevue/fileupload";
+// import { TreeNode } from "primevue/tree";
+
+try {
+  if (!checkRules(readJWT(localStorage.getItem("jwt")!), "Product.Edit")) {
+    await navigateTo("/auth");
+  }
+} catch (e) {
+  await navigateTo("/auth");
+}
+
+const router = useRouter();
+const route = useRoute();
+const id = route.params.id as string;
+
+const state = reactive<{
+  isLoading: boolean;
+  product: Product;
+  newImageBase64: null | string;
+  types: string[];
+  units: string[];
+}>({
+  isLoading: true,
+  product:
+    id == "new"
+      ? {
+          type: "",
+          name: "Новая позиция",
+          qualification: "",
+          unit: "",
+          price1: "0",
+          price2: "0",
+          price3: "0",
+          shortDescription: "",
+          longDescription: "",
+        }
+      : await $fetch(`/api/products/${id}`),
+  newImageBase64: null,
+  types: (await $fetch("/api/types")).types,
+  units: (await $fetch("/api/units")).units, // TODO: fix this(at) red warning!
+});
+// console.log(state);
+
+async function customBase64Uploader(event: FileUploadRemoveUploadedFile) {
+  const file = event.files[0];
+  const reader = new FileReader();
+  const objectURL = URL.createObjectURL(file);
+  let blob = await fetch(objectURL).then((r) => r.blob()); //blob:url
+  console.log(file);
+  reader.readAsDataURL(blob);
+  reader.onloadend = function () {
+    const base64data = reader.result as string;
+    console.log(base64data);
+    state.newImageBase64 = base64data;
+  };
+}
+
+async function onSave() {
+  state.isLoading = true;
+  let imageUploadingResult;
+  if (state.newImageBase64) {
+    imageUploadingResult = await $fetch("/api/admin/img", {
+      method: "POST",
+      body: {
+        jwtToken: localStorage.getItem("jwt"),
+        image: state.newImageBase64,
+      },
+    });
+
+    if (!imageUploadingResult || !imageUploadingResult.hash) {
+      // warn
+      console.error(imageUploadingResult);
+      state.isLoading = false;
+      return;
+    }
+  }
+
+  const productResult = await $fetch("/api/admin/product", {
+    method: "POST",
+    body: {
+      jwtToken: localStorage.getItem("jwt"),
+      newProduct: {
+        ...state.product,
+        hashImage: imageUploadingResult?.hash || state.product.hashImage,
+      } as Product,
+    },
+  });
+
+  console.log(productResult);
+
+  await navigateTo("/admin/product/list");
+  // state.isLoading = false;
+}
+
+state.isLoading = false;
+</script>
+
+<template>
+  <ClientOnly>
+    <BlockUI :blocked="state.isLoading">
+      <div class="content-wall">
+        <table>
+          <tr>
+            <td>
+              <Image
+                :src="'/pictures/' + state.product.hashImage"
+                height="107"
+              />
+            </td>
+            <td>
+              <FileUpload
+                auto
+                mode="basic"
+                name="demo[]"
+                accept="image/*"
+                customUpload
+                @uploader="customBase64Uploader"
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>Наименование</td>
+            <td>
+              <InputText
+                placeholder="Наименование"
+                v-model="state.product.name"
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>Тип продукции</td>
+            <td>
+              <Dropdown v-model="state.product.type" :options="state.types" />
+            </td>
+          </tr>
+          <tr>
+            <td>Единицы измерения</td>
+            <td>
+              <Dropdown v-model="state.product.unit" :options="state.units" />
+            </td>
+          </tr>
+          <tr>
+            <td>Цена</td>
+            <td>
+              <table>
+                <tr>
+                  <td>1</td>
+                  <td>2</td>
+                  <td>3</td>
+                </tr>
+                <tr>
+                  <td>
+                    <InputText v-model="state.product.price1" />
+                  </td>
+                  <td>
+                    <InputText v-model="state.product.price2" />
+                  </td>
+                  <td>
+                    <InputText v-model="state.product.price3" />
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+          <tr>
+            <td>Короткое описание</td>
+            <td><Editor v-model="state.product.shortDescription" /></td>
+          </tr>
+          <tr>
+            <td>Длинное описание</td>
+            <td><Editor v-model="state.product.longDescription" /></td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              <Button
+                label="Сохранить"
+                icon="pi pi-check"
+                @click="onSave"
+                :disabled="state.isLoading"
+              />
+            </td>
+          </tr>
+        </table>
+      </div>
+    </BlockUI>
+  </ClientOnly>
+</template>
---
+++ pages/admin/product/list.vue
@@ -0,0 +1,124 @@
+<script setup lang="ts">
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+import InputText from "primevue/inputtext";
+
+try {
+  if (!checkRules(readJWT(localStorage.getItem("jwt")!), "Product.Edit")) {
+    await navigateTo("/auth");
+  }
+} catch (e) {
+  await navigateTo("/auth");
+}
+
+const loading = ref(false);
+
+const lazyParams = reactive({
+  where: {
+    name: {
+      value: "",
+      matchMode: "",
+    },
+  },
+  take: 5,
+  skip: 0,
+  orderBy: "asc",
+});
+
+const products = ref();
+const amount = ref();
+
+type Data = (
+  | {
+      id: string;
+      name: string;
+    }[]
+  | {
+      _count: {
+        id: number;
+      };
+    }
+)[];
+
+async function lazyLoadData() {
+  loading.value = true;
+
+  const data: Data = await APIHandler.getList({
+    jwtToken: localStorage.getItem("jwt")!,
+    filters: lazyParams,
+  });
+
+  products.value = data[0];
+  amount.value = data[1];
+  loading.value = false;
+}
+
+const onEvent = async (event) => {
+  lazyParams.take = event.rows;
+  lazyParams.skip = event.first;
+
+  lazyParams.where = event.filters;
+  await lazyLoadData();
+};
+
+async function onPage(event) {
+  await onEvent(event);
+}
+async function onSort(event) {
+  await onEvent(event);
+}
+async function onFilter(event) {
+  await onEvent(event);
+}
+
+async function openPage(event) {
+  await navigateTo(`/admin/product/${event.data.id}`);
+}
+
+const filters = ref({
+  name: { value: "", matchMode: "" },
+});
+
+await lazyLoadData();
+</script>
+
+<template>
+  <DataTable
+    paginator
+    lazy
+    v-model:filters="filters"
+    :value="products"
+    :rowsPerPageOptions="[5, 10, 20, 50]"
+    :rows="5"
+    :totalRecords="amount._count.id"
+    paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
+    currentPageReportTemplate="от {first} до {last} из {totalRecords}"
+    tableStyle="min-width: 50rem"
+    filterDisplay="row"
+    :loading="loading"
+    dataKey="id"
+    :globalFilterFields="['name']"
+    selectionMode="single"
+    @page="onPage($event)"
+    @sort="onSort($event)"
+    @filter="onFilter($event)"
+    @rowSelect="openPage"
+    removableSort
+  >
+    <Column header="Изображение">
+      <template #body="slotProps">
+        <img :src="'/pictures/' + slotProps.data.hashImage" height="40" />
+      </template>
+    </Column>
+    <Column field="name" header="Наименование">
+      <template #filter="{ filterModel, filterCallback }">
+        <InputText
+          type="text"
+          v-model="filterModel.value"
+          @keydown.enter="filterCallback()"
+          class="p-column-filter"
+          placeholder="Search"
+        /> </template
+    ></Column>
+  </DataTable>
+</template>
