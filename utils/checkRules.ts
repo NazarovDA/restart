401a195727305ff977cebff368c5ef7ee2e6755e
NazarovDA <d.n.0@bk.ru>
2023-07-18 19:41:43 +0300

    rule checking changed

--- utils/checkRules.ts
+++ utils/checkRules.ts
@@ -1,10 +1,11 @@
 import { jwtPayload } from "types/jwtPayload";
 
-const rules = [...new Set(["admin", "User.Manage", "Product.Create", "Product.Edit"] as const)] as const;
+const rules = [...new Set(["admin", "User.Manage", "Product.Create", "Product.Edit", "Settings.Edit"] as const)] as const;
 
 export default function checkRules(data: jwtPayload, rule: typeof rules[number]): boolean {
     if (data.exp < Date.now()/1000) { return false }
     if (data.status == "admin") { return true }
     if ((rule == "Product.Create" || rule == "Product.Edit") && (data.status == "editor")) { return true }
+    if (rule == "Settings.Edit" && (data.status == "editor")) { return true }
     return false;
 }
